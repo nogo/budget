5f1010c9a297be7936d03ec7706627bd586f1c7c
Danilo Kühn <danilo@kuehns.club>
2025-04-21 11:15:59 +0200

    UPDATE prisma migration command to use 'dev' environment; ADD index on transaction date

--- src/service/review.ts
+++ src/service/review.ts
@@ -2,23 +2,37 @@ import { queryOptions } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
 import dayjs from "dayjs";
 import { z } from "zod";
+import { db } from "~/utils/db";
 
-const reviewYearlySchema = z
+/*
+export const reviewYears = createServerFn().handler(async () => {
+  return await db.reviewYears.findMany();
+});
+
+export const reviewYearsQueryOptions = () => {
+  return queryOptions({
+    queryKey: ["review"],
+    queryFn: () => reviewYears(),
+  });
+};
+
+const reviewYearMonthSchema = z
   .string()
   .optional()
   .transform((d) => dayjs.utc(d, "YYYY", true))
   .default(() => dayjs().format("YYYY"));
 
-export const reviewYearly = createServerFn()
-  .validator(reviewYearlySchema)
+export const reviewYearMonth = createServerFn()
+  .validator(reviewYearMonthSchema)
   .handler(async ({ data }) => {});
 
-export const reviewYearlyQueryOptions = (year: number) => {
+export const reviewYearMonthQueryOptions = (year: number) => {
   year = year || dayjs().year();
   const yearString = year.toString();
 
   return queryOptions({
     queryKey: ["review", yearString],
-    queryFn: () => reviewYearly({ data: yearString }),
+    queryFn: () => reviewYearMonth({ data: yearString }),
   });
 };
+*/
