4b74e54b292c233a210a2b9c7092b33e1a985ce4
Danilo Kühn <danilo@kuehns.club>
2025-06-09 13:13:07 +0200

    add favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -32,24 +32,19 @@ export const Route = createRootRouteWithContext<{
     links: [
       { rel: "stylesheet", href: appCss },
       {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png",
-      },
-      {
         rel: "icon",
         type: "image/png",
         sizes: "32x32",
-        href: "/favicon-32x32.png",
+        href: "/img/favicon-32x32.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "16x16",
-        href: "/favicon-16x16.png",
+        href: "/img/favicon-16x16.png",
       },
-      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
-      { rel: "icon", href: "/favicon.ico" },
+      { rel: "manifest", href: "/site.webmanifest", color: "#733e0a" },
+      { rel: "icon", href: "/img/favicon.ico" },
     ],
   }),
   errorComponent: (props) => {
